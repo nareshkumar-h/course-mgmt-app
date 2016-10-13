afc03d03a403a85774984c797be22d497aa679bd
Janani <jananise@gmail.com>
2016-10-13 16:57:37 +0530

    Added modulo api

--- src/main/java/com/naresh/ArithmeticUtil.java
+++ src/main/java/com/naresh/ArithmeticUtil.java
@@ -5,16 +5,20 @@ public class ArithmeticUtil {
 	public int add(int a, int b) {
 		return a + b;
 	}
-	
+
 	public int sub(int a, int b) {
 		return a - b;
 	}
-	
+
 	public int div(int a, int b) {
-		return a /b;
+		return a / b;
 	}
-	
+
 	public int mul(int a, int b) {
-		return a*b;
+		return a * b;
+	}
+
+	public int mod(int a, int b) {
+		return a % b;
 	}
 }
